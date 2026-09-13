7d2e9f7ea934e0dfb1301f193f3f1c479b155388
Abdulkhan78680 <144554667+Abdulkhan78680@users.noreply.github.com>
2026-09-14 01:04:09 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,22 +1,26 @@
-function largest(){
-    let a=[4,55,44,97,46]
-    let max=a[0]
-
-    for(let i = 1; i<a.length; i++){
-        if(a[i]>max){
-            max=a[i]
+function largest(a) {
+    let max = a[0];
+    for(let i = 1; i < a.length; i++) {
+        if(a[i] > max) {
+            max = a[i];
+        }
     }
-        }console.log("Maximum num",max)   
+
+    console.log("Maximum num", max);
 }
 
+let numbers = [4, 55, 44, 97, 46];
 
-function sum(){
-    let a=[1,3,4,6]
+
+
+
+function sum(b){
+    // let b=[1,3,4,6]
     let sum=0;
 
-    for (let i=0; i<a.length; i++){
+    for (let i=0; i<b.length; i++){
     
-            sum=sum+a[i]  
+            sum=sum+b[i]  
         
     }
         console.log("sum of all eliments",sum) 
@@ -25,12 +29,11 @@ function sum(){
 
 // find the odd number 
 
-function odd(){
-
-let a=[1,2,3,4,5,8,7,16,9,8,17] 
-let count=0;
-for(let i=0; i<a.length; i++){
-    if(a[i]%2!==0){
+function odd(c){
+// let c=[1,2,3,4,5,8,7,16,9,8,17] 
+let count=0;  
+for(let i=0; i<c.length; i++){
+    if(c[i]%2!==0){
         count=count+1;
 }
 
